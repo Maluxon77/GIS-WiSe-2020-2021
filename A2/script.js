66795e157c22adcf0665875011e68aff90d08e0f
Finn <finn.ganzz@gmail.com>
2020-11-09 12:11:28 +0100

    Testing

--- A2/script.js
+++ A2/script.js
@@ -1,3 +1,5 @@
 "use strict";
 console.log("Hi World");
+let five = 5;
+console.log(five * five);
 //# sourceMappingURL=script.js.map
--- A2/script.ts
+++ A2/script.ts
@@ -1,1 +1,3 @@
 console.log("Hi World");
+let five: number = 5;
+console.log(five * five);
