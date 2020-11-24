1cd7b3bbb6fc0f2306ae77eb7d2c0cf3426634f9
Finn <finn.ganzz@gmail.com>
2020-11-24 21:29:02 +0100

    +

--- A2/script.js
+++ A2/script.js
@@ -1,5 +1,22 @@
 "use strict";
-console.log("Hi World");
-let five = 5;
-console.log(five * five);
+document.querySelector("h1").innerHTML = "Meine Überschrift";
+let canvas = document.getElementById("myFirstCanvas");
+let context = canvas.getContext("2d");
+context.lineWidth = 1;
+var recht = {
+    xK: 100,
+    yK: 100,
+    xL: 100,
+    yL: 100
+};
+function createRect() {
+    return context.rect(Math.random() * 1000, Math.random() * 1000, Math.random() * 1000, Math.random() * 1000);
+}
+function drawRect() {
+    context.strokeRect(1000, 1000, 1000, 1000);
+}
+console.log(drawRect);
+context.strokeRect(recht.xK, recht.yK, recht.xL, recht.yL);
+//context.strokeRect(100, 100, 100, 100);  //verschiebenX_verschiebenY_verlängernX_verlängernY
+//context.fillRect(100, 200, 300, 400);
 //# sourceMappingURL=script.js.map
--- A2/script.ts
+++ A2/script.ts
@@ -1,3 +1,38 @@
-console.log("Hi World");
-let five: number = 5;
-console.log(five * five);
+document.querySelector("h1").innerHTML = "Meine Überschrift";
+
+let canvas: HTMLCanvasElement = <HTMLCanvasElement> document.getElementById("myFirstCanvas");
+
+let context: CanvasRenderingContext2D = canvas.getContext("2d");
+
+context.lineWidth = 1;
+
+interface Rechteck {
+    xK: number;
+    yK: number;
+    xL: number;
+    yL: number;
+}
+
+var recht: Rechteck = {
+    xK: 100,
+    yK: 100,
+    xL: 100,
+    yL: 100
+};
+
+function createRect(dasRechteck: Rechteck) {
+    let newRec = {xK: Math.random() * 1000, yk: Math.random() * 100, xL: Math.random() * 100, yL: Math.random() * 1000};
+    return newRec;
+    //return context.rect(xK * 1000, Math.random() * 1000, Math.random() * 1000, Math.random() * 1000);
+
+}
+
+function drawRect(dasRechteck: Rechteck) {
+    context.strokeRect(1000, 1000, 1000, 1000);
+
+}
+
+context.strokeRect(recht.xK, recht.yK, recht.xL, recht.yL);
+
+
+
