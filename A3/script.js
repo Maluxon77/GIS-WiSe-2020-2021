2681e57e6f110736ba99f5f9cdf8cfb7039f364f
Finn <finn.ganzz@gmail.com>
2021-01-20 15:37:24 +0100

    savings

--- A3/script.js
+++ A3/script.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.A3 = void 0;
 const Http = require("http");
+const url = require("url");
 var A3;
 (function (A3) {
     console.log("Starting server");
@@ -16,11 +17,25 @@ var A3;
         console.log("Listening");
     }
     function handleRequest(_request, _response) {
-        console.log("I hear voices!");
-        _response.setHeader("content-type", "text/html; charset=utf-8");
+        console.log("I hear voices!"); // Konsolenausgabe
         _response.setHeader("Access-Control-Allow-Origin", "*");
-        _response.write(_request.url);
         console.log(_request.url);
+        let actUrl = url.parse(_request.url, true);
+        let query = actUrl.query;
+        if (actUrl.pathname == "/html") {
+            _response.setHeader("content-type", "text/html; charset=utf-8");
+            for (let key in query) {
+                console.log(key + ":" + query[key]);
+                _response.write(key + ":" + query[key] + "<br/>");
+            }
+        }
+        else if (actUrl.pathname == "/json") {
+            _response.setHeader("content-type", "application/json");
+            _response.write(JSON.stringify(query));
+        }
+        else {
+            _response.write("please open /html or /json");
+        }
         _response.end();
     }
 })(A3 = exports.A3 || (exports.A3 = {}));
