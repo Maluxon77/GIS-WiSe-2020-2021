4a0d3a389bd52bae6e895bc696b08b43c7befafc
Finn <finn.ganzz@gmail.com>
2021-01-20 15:43:57 +0100

    Ending Test

--- A3/serverTest.js
+++ A3/serverTest.js
@@ -1,47 +1,17 @@
 "use strict";
 var A_3;
 (function (A_3) {
-    //let url: string = "http://localhost:8100";
-    let url = "https://gisaufgabe2020.herokuapp.com";
     let formData = new FormData(document.forms[0]);
-    let answerSec = document.getElementById("answerSec");
-    let btSend = document.getElementById("sendHTML");
-    btSend.addEventListener("click", sendHTML);
-    let btSendJSON = document.getElementById("sendJSON");
-    btSendJSON.addEventListener("click", sendJSON);
-    let btSendPostJSON = document.getElementById("sendPostJSON");
-    btSendPostJSON.addEventListener("click", sendPostJSON);
-    async function sendHTML() {
-        let response = await send(url + "/html");
-        let text = await response.text();
-        answerSec.innerHTML = "ServerAntwort:<br/>" + text;
-    }
-    async function sendJSON() {
-        let response = await send(url + "/json");
-        let json = await response.json();
-        console.log("Answer:");
-        console.log(json);
-        answerSec.innerHTML = "<pre>" + JSON.stringify(json, undefined, 2) + "</pre>";
-    }
-    async function send(_url) {
+    let btSend = document.getElementById("send");
+    btSend.addEventListener("click", send);
+    async function send() {
+        let url = "https://gis2020jw.herokuapp.com/";
         let query = new URLSearchParams(formData);
-        _url = _url + "?" + query.toString();
-        let response = await fetch(_url);
-        return response;
-    }
-    async function sendPostJSON() {
-        let data = JSON.stringify(Object.fromEntries(formData));
-        let response = await fetch(url, {
-            method: "POST",
-            headers: {
-                "Content-Type": "text/plain",
-            },
-            body: data
-        });
-        let json = await response.json();
+        url = url + "?" + query.toString();
+        let response = await fetch(url);
+        let text = await response.text();
         console.log("Answer:");
-        console.log(json);
-        answerSec.innerHTML = "<pre>" + JSON.stringify(json, undefined, 2) + "</pre>";
+        console.log(text);
     }
 })(A_3 || (A_3 = {}));
 //# sourceMappingURL=serverTest.js.map
--- A3/serverTest.ts
+++ A3/serverTest.ts
@@ -1,52 +1,17 @@
 
 namespace A_3 {
-      //let url: string = "http://localhost:8100";
-      let url: string = "https://gisaufgabe2020.herokuapp.com";
+    let formData: FormData = new FormData(document.forms[0]);
 
-      let formData: FormData = new FormData(document.forms[0]);
-      let answerSec: HTMLParagraphElement = <HTMLParagraphElement>document.getElementById("answerSec");
-  
-      let btSend: HTMLButtonElement = <HTMLButtonElement>document.getElementById("sendHTML");
-      btSend.addEventListener("click", sendHTML);
-      let btSendJSON: HTMLButtonElement = <HTMLButtonElement>document.getElementById("sendJSON");
-      btSendJSON.addEventListener("click", sendJSON);
-      let btSendPostJSON: HTMLButtonElement = <HTMLButtonElement>document.getElementById("sendPostJSON");
-      btSendPostJSON.addEventListener("click", sendPostJSON);
-  
-      async function sendHTML(): Promise<void> {
-          let response: Response = await send(url + "/html");
-          let text: string = await response.text();
-          answerSec.innerHTML = "ServerAntwort:<br/>" + text;
-      }
-  
-      async function sendJSON(): Promise<void> {
-          let response: Response = await send(url + "/json");
-          let json: JSON = await response.json();
-          console.log("Answer:");
-          console.log(json);
-          answerSec.innerHTML = "<pre>" + JSON.stringify(json, undefined, 2) + "</pre>";
-      }
-  
-      async function send(_url: string): Promise<Response> {
-          let query: URLSearchParams = new URLSearchParams(<any>formData);
-          _url = _url + "?" + query.toString();
-          let response: Response = await fetch(_url);
-          return response;
-      }
-  
-      async function sendPostJSON(): Promise<void> {
-          let data: string = JSON.stringify(Object.fromEntries(formData));
-          let response: Response = await fetch(url, {
-              method: "POST",
-              headers: {
-                  "Content-Type": "text/plain",
-              },
-              body: data
-          });
-          let json: JSON = await response.json();
-          console.log("Answer:");
-          console.log(json);
-          answerSec.innerHTML = "<pre>" + JSON.stringify(json, undefined, 2) + "</pre>";
-  
-      }
+    let btSend: HTMLButtonElement = <HTMLButtonElement>document.getElementById("send");
+    btSend.addEventListener("click", send);
+
+    async function send(): Promise<void> {
+        let url: string = "https://gis2020jw.herokuapp.com/";
+        let query: URLSearchParams = new URLSearchParams(<any>formData);
+        url = url + "?" + query.toString();
+        let response: Response = await fetch(url);
+        let text: string = await response.text();
+        console.log("Answer:");
+        console.log(text);
+    }
 }
