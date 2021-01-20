e39b3d7f17eb8400fc4c7234985a89dde5aaa0ea
Finn <finn.ganzz@gmail.com>
2021-01-20 15:49:37 +0100

    switched

--- A3/serverTest.js
+++ A3/serverTest.js
@@ -1,17 +1,42 @@
 "use strict";
-var A_3;
-(function (A_3) {
-    let formData = new FormData(document.forms[0]);
-    let btSend = document.getElementById("send");
-    btSend.addEventListener("click", send);
-    async function send() {
-        let url = "https://gis2020jw.herokuapp.com/";
-        let query = new URLSearchParams(formData);
-        url = url + "?" + query.toString();
-        let response = await fetch(url);
-        let text = await response.text();
-        console.log("Answer:");
-        console.log(text);
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.A3 = void 0;
+const Http = require("http");
+const url = require("url");
+var A3;
+(function (A3) {
+    console.log("Starting server");
+    let port = Number(process.env.PORT);
+    if (!port)
+        port = 8100;
+    let server = Http.createServer();
+    server.addListener("request", handleRequest);
+    server.addListener("listening", handleListen);
+    server.listen(port);
+    function handleListen() {
+        console.log("Listening");
     }
-})(A_3 || (A_3 = {}));
+    function handleRequest(_request, _response) {
+        console.log("I hear voices!"); // Konsolenausgabe
+        _response.setHeader("Access-Control-Allow-Origin", "*");
+        console.log(_request.url);
+        let actUrl = url.parse(_request.url, true);
+        let query = actUrl.query;
+        if (actUrl.pathname == "/html") {
+            _response.setHeader("content-type", "text/html; charset=utf-8");
+            for (let key in query) {
+                console.log(key + ":" + query[key]);
+                _response.write(key + ":" + query[key] + "<br/>");
+            }
+        }
+        else if (actUrl.pathname == "/json") {
+            _response.setHeader("content-type", "application/json");
+            _response.write(JSON.stringify(query));
+        }
+        else {
+            _response.write("waiting");
+        }
+        _response.end();
+    }
+})(A3 = exports.A3 || (exports.A3 = {}));
 //# sourceMappingURL=serverTest.js.map
--- A3/serverTest.ts
+++ A3/serverTest.ts
@@ -1,17 +1,45 @@
 
-namespace A_3 {
-    let formData: FormData = new FormData(document.forms[0]);
+import * as Http from "http";
+import * as url from "url";
 
-    let btSend: HTMLButtonElement = <HTMLButtonElement>document.getElementById("send");
-    btSend.addEventListener("click", send);
+export namespace A3 {
+    console.log("Starting server");
+    let port: number = Number(process.env.PORT); 
+    if (!port)
+        port = 8100; 
 
-    async function send(): Promise<void> {
-        let url: string = "https://gis2020jw.herokuapp.com/";
-        let query: URLSearchParams = new URLSearchParams(<any>formData);
-        url = url + "?" + query.toString();
-        let response: Response = await fetch(url);
-        let text: string = await response.text();
-        console.log("Answer:");
-        console.log(text);
+    let server: Http.Server = Http.createServer(); 
+    server.addListener("request", handleRequest); 
+    server.addListener("listening", handleListen);
+    server.listen(port); 
+
+    function handleListen(): void {
+        console.log("Listening");
     }
+
+
+    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
+        console.log("I hear voices!"); // Konsolenausgabe
+        _response.setHeader("Access-Control-Allow-Origin", "*");
+        
+        
+        console.log(_request.url);
+          
+        let actUrl = url.parse(_request.url, true);
+        let query = actUrl.query;
+        if (actUrl.pathname == "/html") {
+                _response.setHeader("content-type", "text/html; charset=utf-8"); 
+                for (let key in query) {
+                    console.log(key + ":" + query[key]);
+                    _response.write(key + ":" + query[key] + "<br/>");
+                }
+            } else if (actUrl.pathname == "/json") {
+                _response.setHeader("content-type", "application/json"); 
+                _response.write(JSON.stringify(query));
+            } else {
+                _response.write("waiting");
+            }
+        _response.end(); 
+        }
+
 }
